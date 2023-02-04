7703612d5b140983b16a781d6f62c4ff34aa30bf
oxau <notoriousact@gmail.com>
2023-02-04 14:00:59 -0800

    commit

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    workhours = int(input('Hours worked: '))
+    reg_hours = 40
+    reg_rate = 18.25
+    o_rate = 27.78
+    if (workhours > 40):
+        reg_charge = reg_rate * 40
+        overtime_charge =  (workhours - 40)*o_rate
+        total_wages = ((workhours - 40)*o_rate) + reg_charge
+    elif (workhours <= 40):
+        reg_charge = (reg_rate * workhours)
+        overtime_charge = 0
+        total_wages = (workhours*reg_rate)
+
+    print ('Regular Charge: ' + str(reg_charge))
+    print ('Overtime Charge: ' + str(overtime_charge))
+    print ('Total Wage: ' + str(total_wages))
     pass
 
 
